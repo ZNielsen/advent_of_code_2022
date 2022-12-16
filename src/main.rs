200a61a055dfbfdf682bcf63d5de4c5b5f966f95
Zach Nielsen <zach@zachn.me>
2022-12-16 13:10:39 -0800

    day2 part 1, need to pause

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
-use std::collections::BinaryHeap;
+use std::collections::{HashMap, BinaryHeap};
 
 fn main() {
-    day_1();
+    // day_1();
+    day_2();
+}
+
+#[derive(Copy, Clone)]
+enum RPSResult {
+    Loss = 0,
+    Draw = 3,
+    Win = 6,
+}
+fn day_2() {
+    let points_table = HashMap::<&str, u64>::from([
+        ("A", 1),
+        ("B", 2),
+        ("C", 3),
+        ("X", 1),
+        ("Y", 2),
+        ("Z", 3),
+    ]);
+    let results_table = HashMap::<&str, RPSResult>::from([
+        ("A X", RPSResult::Draw),
+        ("A Y", RPSResult::Win),
+        ("A Z", RPSResult::Loss),
+        ("B X", RPSResult::Loss),
+        ("B Y", RPSResult::Draw),
+        ("B Z", RPSResult::Win),
+        ("C X", RPSResult::Win),
+        ("C Y", RPSResult::Loss),
+        ("C Z", RPSResult::Draw),
+    ]);
+
+    let filename = String::from("input_files/day2");
+    let contents = std::fs::read_to_string(filename).unwrap();
+
+    let mut total_points: u64 = 0;
+    for matchup in contents.split("\n") {
+        if matchup.is_empty() { continue; }
+        let mut itr = matchup.split(" ");
+        let _them = itr.next().unwrap();
+        let me = itr.next().unwrap();
+
+        let match_total = points_table[me] + results_table[matchup] as u64;
+        total_points += match_total;
+    }
+
+    println!("{}", total_points);
 }
 
 fn day_1() {
